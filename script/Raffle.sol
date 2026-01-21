bf9ea2faa105abdbecb0a9ef70c4f715ef0d9c9c
ha19142 <ha19142@essex.ac.uk>
2026-01-21 20:59:55 +0000

    feat: implement netspec and initial function signatures

--- script/Raffle.sol
+++ script/Raffle.sol
@@ -1,5 +1,30 @@
-//SPDX-Licence-Identifier: MIT
+//SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.19;
 
-contract Raffle{}
+/** 
+ * @title A sample Raffle contract
+ * @author Hassan Ahmed
+ * @notice This contract is for creating a sample raffle
+ * @dev Implements Chainlink VRFv2.5
+*/
+
+contract Raffle{
+    uint256 private immutable I_ENTRANCEFEE;
+
+    constructor(uint256 entranceFee) {
+        I_ENTRANCEFEE = entranceFee;
+    }
+
+    function enterRaffle()public payable {
+        
+    }
+
+    function pickWinner()public {}
+
+    function getEntranceFee()external view returns(uint256){
+        return I_ENTRANCEFEE;
+    }
+
+
+}

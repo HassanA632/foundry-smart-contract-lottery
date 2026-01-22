6e1e7f082929b1d1520237fdae05aaa7e9c79a25
ha19142 <ha19142@essex.ac.uk>
2026-01-22 23:15:20 +0000

    feat: update raffle contract structure and add solidity ordering guidelines

---
+++ SolidityOrder.md
@@ -0,0 +1,28 @@
+Contract elements should be laid out in the following order:
+
+1. Pragma statements
+2. Import statements
+3. Events
+4. Errors
+5. Interfaces
+6. Libraries
+7. Contracts
+
+Inside each contract, library or interface, use the following order:
+
+1. Type declarations
+2. State variables
+3. Events
+4. Errors
+5. Modifiers
+6. Functions
+
+Functions should be grouped according to their visibility and ordered:
+
+1. constructor
+2. receive function (if exists)
+3. fallback function (if exists)
+4. external
+5. public
+6. internal
+7. private
--- script/Raffle.sol
+++ script/Raffle.sol
@@ -2,29 +2,45 @@
 
 pragma solidity 0.8.19;
 
-/** 
+/**
  * @title A sample Raffle contract
  * @author Hassan Ahmed
  * @notice This contract is for creating a sample raffle
  * @dev Implements Chainlink VRFv2.5
-*/
+ */
 
-contract Raffle{
+contract Raffle {
+    /* Errors */
+    error Raffle__SendMoreToEnterRaffle();
+    //     ^^^ good practise, place contract name before error
+
     uint256 private immutable I_ENTRANCEFEE;
+    address payable[] private s_players;
+
+    /**Events */
+    event RaffleEntered(address indexed player);
 
     constructor(uint256 entranceFee) {
         I_ENTRANCEFEE = entranceFee;
     }
 
-    function enterRaffle()public payable {
-        
+    function enterRaffle() public payable {
+        // require(msg.value >= I_ENTRANCEFEE, "Not enough ETH sent!");
+
+        //require(msg.value >= I_ENTRANCEFEE, SendMoreToEnterRaffle()); << Only ^0.8.26
+
+        if (msg.value < I_ENTRANCEFEE) {
+            //Still most gas efficient way
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+
+        emit RaffleEntered(msg.sender);
     }
 
-    function pickWinner()public {}
+    function pickWinner() public {}
 
-    function getEntranceFee()external view returns(uint256){
+    function getEntranceFee() external view returns (uint256) {
         return I_ENTRANCEFEE;
     }
-
-
 }
